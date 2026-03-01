5fa9c006965398ba925985de1e2d3fd6626c584a
CarlosMPatino7822 <carlosmariopatinoramirez@gmail.com>
2026-03-01 18:53:40 -0500

    Se actualizo para poder dockerizar y subir a render

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,15 +3,31 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD as string,
-    {
-        host: process.env.DB_HOST,
+const databaseUrl = process.env.DATABASE_URL;
+
+console.log("DATABASE_URL:", databaseUrl);
+
+const sequelize = databaseUrl
+    ? new Sequelize(databaseUrl, {
         dialect: 'postgres',
+        protocol: 'postgres',
         logging: false,
-    }
-);
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    })
+    : new Sequelize(
+        process.env.DB_NAME as string,
+        process.env.DB_USER as string,
+        process.env.DB_PASSWORD as string,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            logging: false,
+        }
+    );
 
 export default sequelize;
