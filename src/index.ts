36052768a266c717a4ce647d6f43b4e2b8145443
Carlos Andres Lopez Perez <145300252+reizor1781@users.noreply.github.com>
2026-03-01 21:18:31 -0500

    Restrict CORS to specific frontend origin
    Solucion de errores

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import routes from './routes';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors( { origin: 'https://cyc-motorcenter-frontend.onrender.com/' } ));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/api', routes);
